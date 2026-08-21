ef67a405cccfed1f7566fe7ff0007273e820f05b
Murillo Lima <contato@murillolimadev.com.br>
2026-08-21 08:58:49 -0300

    msg whatsapp

--- resources/views/home/layout/app.blade.php
+++ resources/views/home/layout/app.blade.php
@@ -20,8 +20,38 @@
     <!-- Icon Font Stylesheet -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+<!-- BTN FLUANTE -->
+<style>
+.botao-flutuante {
+  position: fixed;
+  bottom: 20px; /* Distância do rodapé */
+  right: 20px; /* Distância da lateral direita */
+  width: 60px;
+  height: 60px;
+  color: white;
+  border-radius: 50px; /* Deixa o botão redondo */
+  text-align: center;
+  font-size: 30px;
+  box-shadow: 2px 2px 5px rgba(0,0,0,0.3); /* Sombra para destaque */
+  z-index: 9999; /* Garante que ficará na frente de tudo */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  transition: all 0.3s ease;
+}
+.botao-flutuante img{
+height: 40px;
+width: 40px;
+}
+/* Efeito ao passar o mouse */
+.botao-flutuante:hover {
+  background-color: #128c7e;
+  transform: scale(1.1); /* Dá um leve aumento no tamanho */
+}
+</style>
 
-    <!-- Customized Bootstrap Stylesheet -->
+<!-- Customized Bootstrap Stylesheet -->
     <link href="{{ asset('home/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
@@ -36,10 +66,10 @@
         @yield('content')
         @include('home.layout.footer')
         <!-- Back to Top -->
-        <a href="https://wa.me/5599991940969" target="_blank"
-            class="btn btn-lg btn-primary btn-lg-square back-to-top1 pt-2">
+        <a href="https://wa.me/5599991940969?text=Olá...%20gostaria%20de%20um%20atendimento,%20encontrei%20esse%20whatsapp%20no%20site." target="_blank"
+            class="botao-flutuante">
             {{-- <i class="bi bi-arrow-up"></i> --}}
-            <img src="{{ asset('home/img/wt.png') }}" alt="" style="width: 70px; height: 70px;">
+            <img src="{{ asset('home/img/wt.png') }}" alt="">
         </a>
     </div>
 
@@ -58,3 +88,7 @@
 </body>
 
 </html>
+
+
+
+
